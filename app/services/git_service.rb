752fed4edf4fd54987d6e0115dc92c1acc5b3dfd
aditya pratama <aditya@zeemi.tv>
2017-03-25 14:58:13 +0700

    Add pull repo git service

--- app/services/git_service.rb
+++ app/services/git_service.rb
@@ -1,7 +1,8 @@
 class GitService
-	def initialize(uri, path = './repo')
+	def initialize(uri, branch = 'master', path = './repo')
 		@uri = uri
 		@path = path
+		@branch = branch
 	end
 
 	def clone
@@ -9,4 +10,9 @@ class GitService
 		Git.clone(@uri, repo_name , :path => @path)
 	end
 
+	def pull
+		repo_name = @uri.split('/')[4].split('.')[0]
+		g = Git.open("#{@path}/#{repo_name}", :log => Logger.new(STDOUT))
+		g.pull
+	end
 end
